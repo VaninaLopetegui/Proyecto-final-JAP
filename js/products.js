87f77aa385080dbb7f4c6c46cc040ac034739d08
intiteran <88692187+intiteran@users.noreply.github.com>
2023-08-16 09:11:12 -0300

    Peticion fetch al API

--- js/products.js
+++ js/products.js
@@ -1,11 +1,18 @@
 // Area para codigo de fetch al API.
+let endpoint = "https://japceibal.github.io/emercado-api/cats/cat.json";
+fetch(endpoint)
 
+  .then(res => res.json())
+  .then(data => {  const objeto_productos = data;
+    categorias(data) 
+    })
+  .catch(err => err)
 
-
-
-
-//PARA HACER - Asignar objeto extraido del fetch a esta constante, objeto pegado directamente en codigo.
-const objeto_productos = JSON.parse(`{"catID":101,"catName":"Autos","products":[{"id":50921,"name":"Chevrolet Onix Joy","description":"Generación 2019, variedad de colores. Motor 1.0, ideal para ciudad.","cost":13500,"currency":"USD","soldCount":14,"image":"img/prod50921_1.jpg"},{"id":50922,"name":"Fiat Way","description":"La versión de Fiat que brinda confort y a un precio accesible.","cost":14500,"currency":"USD","soldCount":52,"image":"img/prod50922_1.jpg"},{"id":50923,"name":"Suzuki Celerio","description":"Un auto que se ha ganado la buena fama por su economía con el combustible.","cost":12500,"currency":"USD","soldCount":25,"image":"img/prod50923_1.jpg"},{"id":50924,"name":"Peugeot 208","description":"El modelo de auto que se sigue renovando y manteniendo su prestigio en comodidad.","cost":15200,"currency":"USD","soldCount":17,"image":"img/prod50924_1.jpg"},{"id":50925,"name":"Bugatti Chiron","description":"El mejor hiperdeportivo de mundo. Producción limitada a 500 unidades.","cost":3500000,"currency":"USD","soldCount":0,"image":"img/prod50925_1.jpg"}]}`)
+  function categorias(data) {
+    console.log(data);
+    let objCategorias = data;
+  }
+    const objeto_productos = JSON.parse(`{"catID":101,"catName":"Autos","products":[{"id":50921,"name":"Chevrolet Onix Joy","description":"Generación 2019, variedad de colores. Motor 1.0, ideal para ciudad.","cost":13500,"currency":"USD","soldCount":14,"image":"img/prod50921_1.jpg"},{"id":50922,"name":"Fiat Way","description":"La versión de Fiat que brinda confort y a un precio accesible.","cost":14500,"currency":"USD","soldCount":52,"image":"img/prod50922_1.jpg"},{"id":50923,"name":"Suzuki Celerio","description":"Un auto que se ha ganado la buena fama por su economía con el combustible.","cost":12500,"currency":"USD","soldCount":25,"image":"img/prod50923_1.jpg"},{"id":50924,"name":"Peugeot 208","description":"El modelo de auto que se sigue renovando y manteniendo su prestigio en comodidad.","cost":15200,"currency":"USD","soldCount":17,"image":"img/prod50924_1.jpg"},{"id":50925,"name":"Bugatti Chiron","description":"El mejor hiperdeportivo de mundo. Producción limitada a 500 unidades.","cost":3500000,"currency":"USD","soldCount":0,"image":"img/prod50925_1.jpg"}]}`)
 // Area para integracion de productos en HTML.
 
 const container = document.querySelector("#productsContainer");
