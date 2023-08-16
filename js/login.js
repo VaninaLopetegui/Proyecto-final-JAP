726c874a615352af7315b7f14f302f1699674bfd
facufer123 <fernandezfacundo10@gmail.com>
2023-08-16 08:15:41 -0300

    guardado en localStorage y redireccionar a login

--- js/login.js
+++ js/login.js
@@ -4,12 +4,15 @@ document.getElementById("loginForm").addEventListener("submit",(e)=>{
     const username=document.getElementById("username").value;
     const password=document.getElementById("password").value;
     
+    if(username!=="" && password!==""){
     localStorage.setItem("loggedIn","true");
     localStorage.setItem("username",username);
-    
-    if(username!=="" && password!==""){
     location.href="index.html";
     }else{alert("Usuario invalido");
     }
-    
+});
+    document.addEventListener("DOMContentLoaded",()=>{
+        if(localStorage.getItem("loggedIn")!== "true"){
+        location.href="login.html"
+    }
     });
